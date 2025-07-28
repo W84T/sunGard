2c36c796624825d406ee034048ad4a9b44ebbfdc
w84t <yamen.zaiter24@gmail.com>
2025-07-28 16:32:04 +0300

    add sungard locale

---
+++ lang/en/sungard_branch.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'form' => [
+        'name' => 'Name',
+        'address' => 'Address',
+    ],
+    'table' => [
+        'name' => 'Name',
+        'address' => 'Address',
+        'created_at' => 'Created At',
+        'updated_at' => 'Updated At',
+    ],
+];
